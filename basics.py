14fb3726200a493fb2003d23efe920941ca30e2a
FasoleWarior <45045665+FasoleWarior@users.noreply.github.com>
2021-01-06 17:38:46 +0200

    Update basics.py

--- basics.py
+++ basics.py
@@ -1,16 +1,20 @@
-#  This is a little introduction to Neural Networks, how they work and a little bit of basic code
+"""
+  This is a little introduction to Neural Networks, how they work and a little bit of basic code
 
-# A Neural Network is a learning system which resembles the human brain, built with lots of elements called neurons or perceptrons.
-# Neurons are arranged in layers: input layer, hidden layers and output layer.
-# The input layer takes the input of the model and feeds it to the next layer through connections named weights,
-# and so on, until it reaches the output layer, where it will generate predictions
+ A Neural Network is a learning system which resembles the human brain, built with lots of elements called neurons or perceptrons.
+ Neurons are arranged in layers: input layer, hidden layers and output layer.
+ The input layer takes the input of the model and feeds it to the next layer through connections named weights,
+ and so on, until it reaches the output layer, where it will generate predictions
 
-# The input layer is a vector which contains values like senzor data, coordinates or pixels of an image (and many more examples)
+ The input layer is a vector which contains values like senzor data, coordinates or pixels of an image (and many more examples)
+"""
 
 # This an example of a simple NN, with just the input and output layers, without any hidden layers
 # The input layer contains 3 variables and the NN will output one variable
+
 inputs = [2, 1.4, 0.9]
 weights = [0.4, 0.2, 3]
+
 # The bias is just a neuron with a fixed variable which doesnt have any connections with the prior layer, only with the next one
 bias = 1
 
