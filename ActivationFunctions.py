7e6a5681773618e9eda762cd14429bedb7811373
FasoleWarior <45045665+FasoleWarior@users.noreply.github.com>
2021-01-07 18:09:43 +0200

    Added some Activation Functions

---
+++ ActivationFunctions.py
@@ -0,0 +1,50 @@
+"""
+    This is the file that contains the Activation Functions
+
+    Functions implemented:
+        - ReLU
+    To be implemented:
+        - Sigmoid
+        - Softmax
+        - TanH
+
+"""
+
+import numpy as np
+
+"""
+    The ReLU function outputs the maximum between a value and 0
+        f(x) = max(x, 0)
+    The ReLU function is a simple function, but in can do a lot of things
+"""
+
+def ReLU(layer):
+    return np.array([max(i, 0) for i in layer])
+
+
+"""
+    Sigmoid function looks something like this:
+        f(x) = 1/(1+exp(-x))
+    This function will output values between 0 and 1
+"""
+
+def sigmoid(layer):
+    exp = np.array([np.e**(-x) for x in layer])
+    return np.array([1/(1+x) for x in exp])
+
+"""
+    TanH function looks something like this:
+         tanh(x)= (exp(x)-exp(−x))/(exp(x)+exp(−x))
+    This function will output values between -1 and 1
+"""
+
+def tanh(layer):
+    result = []
+    expMinus = np.array([np.e**(-x) for x in layer])
+    expPlus = np.array([np.e**x for x in layer])
+    for (x, y) in zip(expPlus, expMinus):
+        result.append((x-y)/(x+y))
+    return np.array(result)
+
+
+
