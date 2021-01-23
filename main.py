b6c12647045414de3daf2360bc62e379a177e426
FasoleWarior <45045665+FasoleWarior@users.noreply.github.com>
2021-01-23 16:50:59 +0200

    Changed structure

--- main.py
+++ main.py
@@ -10,37 +10,72 @@ from keras.datasets import mnist  # for later
 
 # Should be working, but it doesn't
 
-iterations = 10
-alpha = 0.1
-t = NeuralNetworkModel(784, 10)
-t.add(Dense(64, "sigmoid"))
-t.add(Dense(10, "relu"))
-t.add(Dense(10, "sigmoid"))
-
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 x_train = x_train.reshape((-1, 784))/255
 x_test = x_test.reshape((-1, 784))/255
 
-def categorize(a):
-    e = np.zeros((10, 1))
-    e[a] = 1
-    return e
+def categorize(j):
+    a = np.zeros((10, 1))
+    a[j] = 1
+    return a
 
-# I'll train it on a small part of the dataset 'cause my laptop can't do more
-# but now I run the entire dataset on colab
-x = x_train[:500]
+
 y = []
-for j in y_train[:500]:
+for j in y_train:
     y.append(categorize(j))
 
-t.train(x_train, y, iterations, alpha, 32, x_test, y_test)
+train = list(zip(x_train, y))
+test = list(zip(x_test, y_test))
 
-""" The Results after running it in colab:
-
-With only 2 layers - it ran better
-t.add(Dense(64, "relu"))
+epochs = 30
+alpha = 0.1
+batch_size = 32
+t = NeuralNetworkModel(784, 10)
+t.add(Dense(64, "sigmoid"))
 t.add(Dense(10, "sigmoid"))
+
+t.train(train, epochs, alpha, batch_size, test)
+
+""" The Results after running it:
+
+On my laptop, with the above structure:
+
+Epoch 1: 1716 / 10000
+Epoch 2: 2849 / 10000
+Epoch 3: 3964 / 10000
+Epoch 4: 4184 / 10000
+Epoch 5: 4299 / 10000
+Epoch 6: 4606 / 10000
+Epoch 7: 4968 / 10000
+Epoch 8: 5094 / 10000
+Epoch 9: 5160 / 10000
+Epoch 10: 5205 / 10000
+Epoch 11: 5243 / 10000
+Epoch 12: 5276 / 10000
+Epoch 13: 5303 / 10000
+Epoch 14: 5317 / 10000
+Epoch 15: 5330 / 10000
+Epoch 16: 5339 / 10000
+Epoch 17: 5350 / 10000
+Epoch 18: 5358 / 10000
+Epoch 19: 5366 / 10000
+Epoch 20: 5373 / 10000
+Epoch 21: 5381 / 10000
+Epoch 22: 5395 / 10000
+Epoch 23: 5404 / 10000
+Epoch 24: 5409 / 10000
+Epoch 25: 5422 / 10000
+Epoch 26: 5424 / 10000
+Epoch 27: 5427 / 10000
+Epoch 28: 5444 / 10000
+Epoch 29: 5436 / 10000
+Epoch 30: 5454 / 10000
+
+Process finished with exit code 0
+
+
+In colab, with relu and sigmoid, 20 epochs
 
 Epoch 1: 2588 / 10000
 Epoch 2: 3056 / 10000
@@ -52,11 +87,6 @@ Epoch 7: 4556 / 10000
 Epoch 8: 4716 / 10000
 Epoch 9: 4866 / 10000
 Epoch 10: 4992 / 10000
-
-Almost 50 %
-
-And after 10 more epochs:
-
 Epoch 11: 5103 / 10000
 Epoch 12: 5231 / 10000
 Epoch 13: 5334 / 10000
